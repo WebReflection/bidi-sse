94b0240ab927cc7e7371183df1fd14d22a93e1c3
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2021-11-27 12:31:04 +0100

    fixed bidi.close() events order

--- esm/server.js
+++ esm/server.js
@@ -24,8 +24,8 @@ class Client extends Class(EventEmitter) {
   close() {
     if (this.readyState !== CLOSED) {
       this.readyState = CLOSED;
-      this._.end();
       this.emit('close');
+      this._.end();
     }
   }
 }
@@ -45,32 +45,110 @@ export default class Server extends EventEmitter {
    * @param {ServerOptions=} options extra options to use
    */
   constructor(path, options = {}) {
-    super();
     const clients = new Map;
-    const values = new SetValues(clients);
     const {parse, stringify} = options.JSON || JSON;
     const headers = options.headers || {};
     if (options.mode === 'cors')
       headers['access-control-allow-origin'] = '*';
 
-    this.handler = handler.bind(this);
-    this._ = {path, headers, parse, stringify, clients, values};
+    super()._ = new SetValues(clients);
+
+    /**
+     * Handles both *express* and regular *http* server as callback.
+     * `bidisse.handler(req, res)` returns `true` if the request was handled.
+     * @param {import('http').IncomingMessage} req the request instace
+     * @param {import('http').ServerResponse} res the response instance
+     * @param {function=} next optional callback to keep working (express)
+     * @returns {boolean} `true` if handling the request, `false` otherwise.
+     */
+    this.handler = (req, res, next) => {
+      const {method, url} = req;
+      if (url.startsWith(path)) {
+        if (method === 'POST') {
+          const i = url.indexOf('?');
+          if (-1 < i) {
+            const searchParams = new URLSearchParams(url.slice(i));
+            if (searchParams.has('bidi-sse')) {
+              const uid = searchParams.get('bidi-sse');
+              if (clients.has(uid)) {
+                const chunks = [];
+                req
+                  .on('data', chunk => { chunks.push(chunk); })
+                  .on('end', () => {
+                    if (!clients.has(uid)) return;
+                    const client = clients.get(uid);
+                    let data;
+                    try { data = parse(chunks.join('')); }
+                    catch ({message}) {
+                      const error = stringify(message);
+                      client._.write(`event: unexpected\ndata: ${error}\n\n`);
+                      return;
+                    }
+                    client.emit('message', data);
+                  })
+                ;
+                res.writeHead(200, headers).end();
+                return true;
+              }
+            }
+          }
+        }
+        else if (
+          method === 'GET' &&
+          url === path &&
+          req.headers.accept === 'text/event-stream'
+        ) {
+          let uid = ''; // what are the odds
+          do { uid = randomUUID({disableEntropyCache: true}); }
+          while (clients.has(uid));
+
+          const client = new Client(res, data => {
+            try { res.write(`data: ${stringify(data)}\n\n`); }
+            catch (err) { client.emit('error', err); }
+          });
+
+          clients.set(uid, client);
+          res
+            .once(
+              'close',
+              () => {
+                clients.delete(uid);
+                client.close();
+              }
+            )
+            .writeHead(200, {
+              ...headers,
+              'connection': 'keep-alive',
+              'cache-control': 'no-cache',
+              'content-type': 'text/event-stream'
+            })
+            .write(`event: bidi-sse\ndata: ${stringify(uid)}\n\n`)
+          ;
+          this.emit('connection', client);
+          return true;
+        }
+      }
+
+      try { return false } finally {
+        if (typeof next === 'function')
+          next();
+      }
+    };
   }
 
 
   /**
    * @type {Set<Client>} all connected clients
    */
-  get clients() { return this._.values; }
+  get clients() { return this._; }
 
   /**
    * Close all connected clients and emit the `close` event.
    */
   async close() {
     const closed = [];
-    const {clients} = this._;
     const once = ({_}) => new Promise($ => _.once('close', $));
-    for (const client of clients.values()) {
+    for (const client of this._) {
       if (client.readyState === OPEN) {
         closed.push(once(client));
         client.close();
@@ -78,88 +156,5 @@ export default class Server extends EventEmitter {
     }
     await Promise.all(closed);
     this.emit('close');
-  }
-}
-
-/**
- * Handles both *express* and regular *http* server as callback.
- * `bidisse.handler(req, res)` returns `true` if the request was handled.
- * @param {import('http').IncomingMessage} req the request instace
- * @param {import('http').ServerResponse} res the response instance
- * @param {function=} next optional callback to keep working (express)
- * @returns {boolean} `true` if handling the request, `false` otherwise.
- */
-function handler(req, res, next) {
-  const {path, headers, parse, stringify, clients} = this._;
-  const {method, url} = req;
-  if (url.startsWith(path)) {
-    if (method === 'POST') {
-      const i = url.indexOf('?');
-      if (-1 < i) {
-        const searchParams = new URLSearchParams(url.slice(i));
-        if (searchParams.has('bidi-sse')) {
-          const uid = searchParams.get('bidi-sse');
-          if (clients.has(uid)) {
-            const chunks = [];
-            req
-              .on('data', chunk => { chunks.push(chunk); })
-              .on('end', () => {
-                if (!clients.has(uid)) return;
-                const client = clients.get(uid);
-                let data;
-                try { data = parse(chunks.join('')); }
-                catch ({message}) {
-                  const error = stringify(message);
-                  client._.write(`event: unexpected\ndata: ${error}\n\n`);
-                  return;
-                }
-                client.emit('message', data);
-              })
-            ;
-            res.writeHead(200, headers).end();
-            return true;
-          }
-        }
-      }
-    }
-    else if (
-      method === 'GET' &&
-      url === path &&
-      req.headers.accept === 'text/event-stream'
-    ) {
-      let uid = ''; // what are the odds
-      do { uid = randomUUID({disableEntropyCache: true}); }
-      while (clients.has(uid));
-
-      const client = new Client(res, data => {
-        try { res.write(`data: ${stringify(data)}\n\n`); }
-        catch (err) { client.emit('error', err); }
-      });
-
-      clients.set(uid, client);
-      res
-        .once(
-          'close',
-          () => {
-            clients.delete(uid);
-            client.close();
-          }
-        )
-        .writeHead(200, {
-          ...headers,
-          'connection': 'keep-alive',
-          'cache-control': 'no-cache',
-          'content-type': 'text/event-stream'
-        })
-        .write(`event: bidi-sse\ndata: ${stringify(uid)}\n\n`)
-      ;
-      this.emit('connection', client);
-      return true;
-    }
-  }
-
-  try { return false } finally {
-    if (typeof next === 'function')
-      next();
   }
 }
